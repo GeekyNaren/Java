168c840f5f393ee1b7692a69582b824462429fe2
Narendra Bhosale <bhosalenarendra15@gmai.com>
2022-10-19 00:44:32 +0530

    linkedlist first program

---
+++ LinkedList/SingleLinkedList/SingleLL.java
@@ -0,0 +1,105 @@
+package LinkedList.SingleLinkedList;
+
+public class SingleLL {
+    Node head;
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    //add fisr, last
+
+        public void addFirst(String data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+    
+    //add last
+    
+        public void addLast(String data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = newNode;
+                return;
+            }
+            
+            Node currNode = head;
+            while(currNode.next != null){
+                currNode = currNode.next;
+            }
+
+            currNode.next = newNode;
+        }
+
+    //print
+
+        public void printList(){
+            if(head == null){
+                System.out.println("List is empty");
+                return;
+            }
+            Node currNode = head;
+            while(currNode != null){
+                System.out.print(currNode.data+ " -> ");
+                currNode = currNode.next;
+            }
+            System.out.println("NULL");
+        }
+
+    //delete node first 
+        public void deleteFirst(){
+            if(head == null){
+                System.out.println("list is empty");
+                return;
+            }
+            head = head.next;
+        }
+        
+    //delete node last
+    
+    public void deleteLast(){
+            if(head == null){
+                System.out.println("list is empty");
+                return;
+            }
+            if(head.next == null){
+                head = null;
+                return;
+            }
+            Node secondLast = head;
+            Node lasNode = head.next;
+            while(lasNode.next != null){
+                lasNode = lasNode.next;
+                secondLast = secondLast.next;
+            }
+            secondLast.next = null;
+        }
+    public static void main(String[] args) {
+        SingleLL list = new SingleLL();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        
+        list.addLast("list");
+        list.printList();
+
+        list.addFirst("this");
+        list.printList();
+
+        list.deleteFirst();
+        list.printList();
+
+        // list.deleteLast();
+        // list.printList();
+    }
+}
